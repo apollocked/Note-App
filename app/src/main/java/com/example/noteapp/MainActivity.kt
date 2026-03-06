19e700a6c0a994576c76635ca989075ea7c2f7b5
apollocked <hamabarznji1990@gmail.com>
2026-03-06 22:52:19 +0300

    Creating The RecyclerView Adapter

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -4,15 +4,26 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.room.NoteViewModel
+import com.example.noteapp.adaptors.NoteAdaptor
 
 class MainActivity : AppCompatActivity() {
+    private val notesAdaptor = NoteAdaptor()
+    private lateinit var recyclerView: RecyclerView
     private lateinit var noteViewModel: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        recyclerView = findViewById(R.id.recycler_view)
+        recyclerView.adapter = notesAdaptor
+        recyclerView.adapter=notesAdaptor
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+
         noteViewModel = ViewModelProvider(
             this,
             ViewModelProvider.AndroidViewModelFactory.getInstance(application)
@@ -20,8 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         noteViewModel.allNotes.observe(this){list ->
             //here we can add the data to the recycler view
-
-
+            notesAdaptor.setNotes(list)
         }
 
     }
--- app/src/main/java/com/example/noteapp/adaptors/NoteAdaptor.kt
+++ app/src/main/java/com/example/noteapp/adaptors/NoteAdaptor.kt
@@ -1,4 +1,50 @@
 package com.example.noteapp.adaptors
 
-class NoteAdaptor {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.room.Note
+import com.example.noteapp.R
+
+class NoteAdaptor()
+    : RecyclerView.Adapter<NoteAdaptor.NoteViewHolder>() {
+private var notesList : MutableList<Note> = mutableListOf()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): NoteViewHolder {
+return NoteViewHolder(
+    LayoutInflater.from(parent.context)
+        .inflate(R.layout.note_item,parent,false))
+    }
+
+    override fun onBindViewHolder(
+        holder: NoteViewHolder,
+        position: Int
+    ) {
+        val note = notesList[position]
+        holder.textViewTitle.text = note.title
+        holder.textViewDescription.text = note.description
+        holder.textViewPriority.text = note.priority.toString()
+    }
+
+
+    override fun getItemCount(): Int = notesList.size
+
+    fun setNotes(notes: MutableList<Note>) {
+        this.notesList = notes
+    }
+
+    inner class NoteViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textViewTitle = view.findViewById<TextView>(R.id.text_view_title)
+        val textViewDescription = view.findViewById<TextView>(R.id.text_view_description)
+         val textViewPriority = view.findViewById<TextView>(R.id.text_view_priority)
+
+    }
+
+
 }
